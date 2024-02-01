16dc1cad9198b19e766ae74feab6bde82bf615a5
pysrxyz <gm@pysr.xyz>
2024-02-01 10:55:16 +0700

    shitzu token with caps

--- src/components/ShitzuToken.tsx
+++ src/components/ShitzuToken.tsx
@@ -1,22 +1,34 @@
 "use client";
 
+import { useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { Mesh } from "three";
+import { Color, Group } from "three";
 
 export default function ShitzuToken() {
-  const ref = useRef<Mesh>(null);
+  const ref = useRef<Group>(null);
+  const logo = useTexture("/shitzu.png");
 
   useFrame((state, delta) => {
     if (!ref.current) return;
 
-    ref.current.rotation.z += delta;
+    ref.current.rotation.z += delta / 2;
   });
 
   return (
-    <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-      <cylinderGeometry args={[1, 1, 0.2, 32]} />
-      <meshBasicMaterial color={"red"} />
-    </mesh>
+    <group ref={ref} rotation={[Math.PI / 2, 0, 0]}>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.1, 0]}>
+        <circleGeometry args={[1, 32]} />
+        <meshBasicMaterial map={logo} />
+      </mesh>
+      <mesh>
+        <cylinderGeometry args={[1, 1, 0.2, 32, 32, true]} />
+        <meshBasicMaterial color={new Color("#31c891")} />
+      </mesh>
+      <mesh rotation={[Math.PI / 2, 0, Math.PI]} position={[0, -0.1, 0]}>
+        <circleGeometry args={[1, 32]} />
+        <meshBasicMaterial map={logo} />
+      </mesh>
+    </group>
   );
 }
