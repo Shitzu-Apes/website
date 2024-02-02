f28a68859b27171505cfbfa8bccfc4e3fb90ee2f
pysrxyz <gm@pysr.xyz>
2024-02-02 23:18:30 +0700

    fix navbar link

--- src/components/Navbars.tsx
+++ src/components/Navbars.tsx
@@ -61,7 +61,7 @@ export default function Navbar() {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <Link
                         key={item.name}
                         href={item.href}
                         className={classNames(
@@ -73,7 +73,7 @@ export default function Navbar() {
                         aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
-                      </a>
+                      </Link>
                     ))}
                   </div>
                 </div>
