b2fc47376158ae4401bf4815734aa4ab92490cd8
pysrxyz <gm@pysr.xyz>
2024-03-28 20:50:25 +0700

    feat: copy image to out folder

---
+++ scripts/copyImages.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+
+const blogsDir = path.join(__dirname, "..", "blogs");
+const publicDir = path.join(__dirname, "..", "out", "blog");
+
+fs.readdir(blogsDir, (err, slugs) => {
+  if (err) throw err;
+
+  slugs.forEach((slug) => {
+    const srcDir = path.join(blogsDir, slug);
+    fs.readdir(srcDir, (err, files) => {
+      if (err) throw err;
+
+      files.forEach((file) => {
+        if (file.match(/thumbnail\.(png|webp|jpeg)$/)) {
+          const srcPath = path.join(srcDir, file);
+          const destPath = path.join(publicDir, slug, file);
+
+          if (!fs.existsSync(path.join(publicDir, slug))) {
+            fs.mkdirSync(path.join(publicDir, slug));
+          }
+
+          fs.copyFileSync(srcPath, destPath);
+        }
+      });
+    });
+  });
+});
