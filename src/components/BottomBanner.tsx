14b592ef7e85f2e457d76f192def3c02121b207b
pysrxyz <gm@pysr.xyz>
2024-05-23 11:06:46 +0700

    feat: add countdown timer to bridge close banner

--- src/components/BottomBanner.tsx
+++ src/components/BottomBanner.tsx
@@ -3,10 +3,38 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { BellAlertIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const closeDate = new Date("2024-06-15");
+function getCountdown() {
+  const countDown = closeDate.getTime() - new Date().getTime();
+
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  return { days, hours, minutes, seconds };
+}
 
 export default function BottomBanner() {
   const [isBannerVisible, setIsBannerVisible] = useState(true);
+  const [countdown, setCountDown] = useState<{
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }>(getCountdown());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(getCountdown());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
 
   return isBannerVisible ? (
     <div className="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8 z-10">
@@ -14,8 +42,15 @@ export default function BottomBanner() {
         <p className="text-sm leading-6 text-white">
           <Link href="/blog/2024-05-15-bridge-poll">
             <BellAlertIcon className="h-5 w-5 inline mx-2" aria-hidden="true" />
-            Hurry up! The bridge will be closed by Mid June 2024.{" "}
-            <span aria-hidden="true">&rarr;</span>
+            Hurry up! The bridge will be closed{" "}
+            {countdown ? (
+              <>
+                in {countdown.days}d {countdown.hours}h {countdown.minutes}m{" "}
+                {countdown.seconds}s<span aria-hidden="true">&rarr;</span>
+              </>
+            ) : (
+              "by June 15th, 2024"
+            )}
           </Link>
         </p>
         <button type="button" className="-m-1.5 flex-none p-1.5">
